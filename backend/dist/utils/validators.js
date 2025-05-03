e55de877bc1664563e608dd16c7156db14d9c158
Lewis Williams <lewiswilliams077@outlook.com>
2025-05-03 18:40:13 +0100

    Update .gitignore to exclude dist directories

--- backend/dist/utils/validators.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.validateEmail = void 0;
-const validateEmail = (email) => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-};
-exports.validateEmail = validateEmail;
